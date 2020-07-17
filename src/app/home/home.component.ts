545ab83aada3ae0d48644aeb8167a066e8919468
Asif Rahman <asrahman@microsoft.com>
2020-07-16 19:20:03 -0700

    enum

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,19 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
 
+enum ComponentSelector {
+  XML = 1,
+  About= 2
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
   public nodes;
-  currentLink = 1;
+  currentLink = this.componentSelector.XML;
   options = {};
 
   constructor(public data: DataService) { 
     this.nodes = data.getTree();
   }
+
+  get componentSelector() { return ComponentSelector; }
 
   async setLink(linkId) {
 		this.currentLink = linkId;
