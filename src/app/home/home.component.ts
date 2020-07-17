02f079b7dd081cbc86f5d152f530ec84bd8fb4df
Asif Rahman <asrahman@microsoft.com>
2020-07-16 18:50:03 -0700

    grid, component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,16 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
   public nodes;
+  currentLink = 1;
   options = {};
 
   constructor(public data: DataService) { 
     this.nodes = data.getTree();
   }
+
+  async setLink(linkId) {
+		this.currentLink = linkId;
+	}
 
   ngOnInit(): void { }
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,10 @@ import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 
 import { TreeModule } from '@circlon/angular-tree-component';
+import { AboutComponent } from '../about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AboutComponent],
   imports: [CommonModule, SharedModule, HomeRoutingModule, TreeModule]
 })
 export class HomeModule {}
